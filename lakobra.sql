9f60038a20e043c4e10d9962e8a26651d1d1c68d
Enekoplaza2004 <eneko.plazava@elorrieta-errekamari.com>
2026-05-15 14:30:39 +0200

    sql

--- lakobra.sql
+++ lakobra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2026 a las 14:11:57
+-- Tiempo de generación: 15-05-2026 a las 14:29:36
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -24,169 +24,55 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `asistencias`
+-- Estructura de tabla para la tabla `evento_tareas`
 --
 
-CREATE TABLE `asistencias` (
+CREATE TABLE `evento_tareas` (
   `id` int(11) NOT NULL,
   `id_evento` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `fecha_hora_entrada` datetime DEFAULT current_timestamp()
+  `nombre_tarea` varchar(100) NOT NULL,
+  `limite_usuarios` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
-
 --
--- Estructura de tabla para la tabla `eventos`
---
-
-CREATE TABLE `eventos` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(150) NOT NULL,
-  `fecha_evento` date NOT NULL,
-  `hora_inicio` time DEFAULT NULL,
-  `aforo_max` int(11) DEFAULT 120,
-  `estado` enum('pendiente','confirmado','cancelado') DEFAULT 'pendiente',
-  `visible_publico` tinyint(1) DEFAULT 0,
-  `txandalaris_max` int(11) DEFAULT 8
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
-
---
--- Estructura de tabla para la tabla `solicitudes_artistas`
+-- Volcado de datos para la tabla `evento_tareas`
 --
 
-CREATE TABLE `solicitudes_artistas` (
-  `id` int(11) NOT NULL,
-  `nombre_artista` varchar(150) NOT NULL,
-  `email_contacto` varchar(100) NOT NULL,
-  `descripcion` text DEFAULT NULL,
-  `fecha_solicitud` datetime DEFAULT current_timestamp(),
-  `estado` enum('pendiente','aceptada','rechazada') DEFAULT 'pendiente'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `turnos`
---
-
-CREATE TABLE `turnos` (
-  `id` int(11) NOT NULL,
-  `id_evento` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `puesto` enum('barra','puerta','limpieza','otros') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `dni` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `qr_token` varchar(100) NOT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `rol` enum('admin','txandalari','socio') DEFAULT 'socio',
-  `solicitud_txandalari` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
+INSERT INTO `evento_tareas` (`id`, `id_evento`, `nombre_tarea`, `limite_usuarios`) VALUES
+(13, 39, 'Taberna (Barra)', 4),
+(14, 39, 'Sarrera (Atea)', 2),
+(15, 39, 'Garbiketa', 2);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `asistencias`
+-- Indices de la tabla `evento_tareas`
 --
-ALTER TABLE `asistencias`
+ALTER TABLE `evento_tareas`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_evento` (`id_evento`,`id_usuario`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `eventos`
---
-ALTER TABLE `eventos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `solicitudes_artistas`
---
-ALTER TABLE `solicitudes_artistas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `turnos`
---
-ALTER TABLE `turnos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_evento` (`id_evento`,`id_usuario`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `dni` (`dni`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `qr_token` (`qr_token`);
+  ADD KEY `id_evento` (`id_evento`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `asistencias`
+-- AUTO_INCREMENT de la tabla `evento_tareas`
 --
-ALTER TABLE `asistencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `eventos`
---
-ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `solicitudes_artistas`
---
-ALTER TABLE `solicitudes_artistas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `turnos`
---
-ALTER TABLE `turnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `evento_tareas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `asistencias`
+-- Filtros para la tabla `evento_tareas`
 --
-ALTER TABLE `asistencias`
-  ADD CONSTRAINT `asistencias_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `asistencias_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `turnos`
---
-ALTER TABLE `turnos`
-  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+ALTER TABLE `evento_tareas`
+  ADD CONSTRAINT `evento_tareas_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
