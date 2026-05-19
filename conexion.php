144946064b34d2f96318ce0c35b9247bc1f66b22
Enekoplaza2004 <eneko.plazava@elorrieta-errekamari.com>
2026-05-19 13:30:53 +0200

    commit backend conexion

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 function conexionBBDD() {
     $host = "localhost";
-    $user = "lakobra1";
-    $pass = "12345";
-    $db   = "lakobra1"; 
+    $user = "root";
+    $pass = "";
+    $db   = "lakobra"; 
 
     $conexion = mysqli_connect($host, $user, $pass, $db);
     if (!$conexion) {
